f0141e3bb71f303c24b6f4c5c14fe31581a820ec
Ondřej Fíla <114986357+ondrejfila6969@users.noreply.github.com>
2025-11-20 13:45:25 +0100

    SERVER - essential things added (#5)

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require("cors");
+
+require("dotenv").config();
+
+// DB CONNECTION
+const mongoose = require("mongoose");
+mongoose
+.connect(String(process.env.MONGODB_URL))
+.then(() => {console.log("Database connected")})
+.catch((err) => {console.error(err)})
+
+
+// ROUTERS
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
