48e6390a70bf66e25ae43982fa0121dbce2bd8bd
tomsa159 <ondrej.tomsicek@student.spsmb.cz>
2025-12-04 13:53:54 +0100

    jsx

---
+++ MobileFE/app/marks.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const marks = () => {
+  return (
+    <View>
+      <Text>marks</Text>
+    </View>
+  )
+}
+
+export default marks
+
+const styles = StyleSheet.create({})
---
+++ MobileFE/app/timeTable.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const timeTable = () => {
+  return (
+    <View>
+      <Text>timeTable</Text>
+    </View>
+  )
+}
+
+export default timeTable
+
+const styles = StyleSheet.create({})
