a1f7c6a409d2b63796a8b6d5609ee81ddbf3dd28
Roman Brdlík <114986487+rbrdlik@users.noreply.github.com>
2025-12-11 13:35:30 +0100

    feat: Implement grades management with CRUD operations and Swagger documentation (#14)
    - Added grades controller with methods to get, create, update, and delete grades.
    - Created grades model using Mongoose.
    - Established routes for grades with Swagger annotations for API documentation.
    - Updated package.json to include necessary dependencies for Swagger and nodemon.
    - Added .gitignore to exclude node_modules and .env files.
    - Cleaned up users route by removing unnecessary comments.

--- server/app.js
+++ server/app.js
@@ -1,29 +1,23 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require("cors");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-require("dotenv").config();
+const { swaggerDocs } = require('./swagger');
 
-// DB CONNECTION
-const mongoose = require("mongoose");
-mongoose
-.connect(String(process.env.MONGODB_URL))
-.then(() => {console.log("Database connected")})
-.catch((err) => {console.error(err)})
-
-
-// ROUTERS
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const gradesRouter = require('./routes/grades');
 
-const app = express();
+var app = express();
 
+swaggerDocs(app);
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,15 +26,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/grades', gradesRouter);
 
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
